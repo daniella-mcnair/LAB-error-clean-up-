5dcf640ea97763f3c9bf3160df23ec27867d8bff
Daniella McNair <daniella.mcnair495@gmail.com>
2023-11-14 12:15:43 -0600

    :(

--- public/main.js
+++ public/main.js
@@ -10,46 +10,45 @@ const htmlStructure = () => {
 
   // import '../styles/main.scss'; // You have to import your styles for them to work. Comment in this line
 
-const startApp = () => {
-  htmlStructure(); // always load first
-  header();
-  startSortingBtn();
-  events(); // always load last
-};
+  const startApp = () => {
+    htmlStructure(); // always load first
+    header();
+    startSortingBtn();
+    events(); // always load last
+  };
 
-const students = [];
-const voldysArmy = []; // starts as an empty array
+  const students = [];
+  const voldysArmy = []; // starts as an empty array
 
-
-const events = () => {
+  const events = () => {
   // get form on the DOM on button click
-  document.querySelector('#start-sorting').addEventListener('click', () => {
+    document.querySelector('#start-sorting').addEventListener('click', () => {
     // put html elements on the DOM on click
-    form(); // form
-    filterBtnRow(); // filter buttons
-    studentAreas(); // students and voldy's army divs
-  });
-
-  // target expel buttons to move to voldys army
-  document
-    .querySelector('#student-container')
-    .addEventListener('click', (e) => {
-      if (e.target.id.includes('expel')) {
-        const [, id] = e.target.id.split('--');
-        const index = students.findIndex((student) => student.id === Number(id));
-
-        // move from one array to another
-        voldysArmy.push(...students.splice(index, 1));
-        // get both sets of students on the DOM
-        studentsOnDom('#students', students);
-        studentsOnDom('#voldy', voldysArmy);
-      }
+      form(); // form
+      filterBtnRow(); // filter buttons
+      studentAreas(); // students and voldy's army divs
     });
 
-  // target filter buttons on Dom
-  document.querySelector('#filter-container').addEventListener('click', (e) => {
-    if (e.target.id.includes('filter')) {
-      const [, house] = e.target.id.split('--');
+    // target expel buttons to move to voldys army
+    document
+      .querySelector('#student-container')
+      .addEventListener('click', (e) => {
+        if (e.target.id.includes('expel')) {
+          const [, id] = e.target.id.split('--');
+          const index = students.findIndex((student) => student.id === Number(id));
+
+          // move from one array to another
+          voldysArmy.push(...students.splice(index, 1));
+          // get both sets of students on the DOM
+          studentsOnDom('#students', students);
+          studentsOnDom('#voldy', voldysArmy);
+        }
+      });
+
+    // target filter buttons on Dom
+    document.querySelector('#filter-container').addEventListener('click', (e) => {
+      if (e.target.id.includes('filter')) {
+        const [, house] = e.target.id.split('--');
 
         if (house === 'all') {
           studentsOnDom('#students', students);
@@ -57,13 +56,13 @@ const events = () => {
           const filter = students.filter((student) => student.house === house);
           studentsOnDom('#students', filter, house);
         }
-    }
-  });
+      }
+    });
 };
 
 
 
-  renderToDOM('#app', domString)
+  renderToDOM('#app', domString);
 };
 
 const header = () => {
@@ -87,7 +86,7 @@ const startSortingBtn = () => {
 
 const studentAreas = () => {
   const domString = `<div id="students">No Students</div>
-  <div id="voldy">No Death Eaters</div>`
+  <div id="voldy">No Death Eaters</div>`;
 
   renderToDOM('#student-container', domString);
 };
@@ -162,7 +161,6 @@ const createId = (array) => {
   }
   return 0;
 };
-
 
 // add form to DOM on start-sorting click.
 // Add events for form after the form is on the DOM
